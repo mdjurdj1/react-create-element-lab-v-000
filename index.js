3de5082d8b1db6c6c2e5a4f360945563cadeb73e
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-04-22 06:59:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Code React element here
+const header = React.createElement("h1", {}, "An Awesome Person")
+const paragraph = React.createElement("p", {}, "Who is learning React")
+
+
+const list = React.createElement("ul", { className: "me__interests"}, 
+  [ 
+    React.createElement('li', {}, 'JavaScript'),
+    React.createElement('li', {}, 'React'),
+    React.createElement('li', {}, 'Movies'),
+    React.createElement('li', {}, 'Ice cream')
+  ])
+
+
+const meInReact = React.createElement("div", { className: "me" }, [header, paragraph, list])
+
+ReactDOM.render(
+  meInReact,
+  document.getElementById('main')
+)
